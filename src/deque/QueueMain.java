51b550f3d119af78f0a46aa18a0e949e46f1a1cd
Kim Dong Yeon <do121712@naver.com>
2024-07-23 22:47:31 +0900

    feat : 자바에서 제공하는 Queue 기능 확인

---
+++ src/deque/QueueMain.java
@@ -0,0 +1,26 @@
+package deque;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        //데이터 추가
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println(queue);
+
+        //꺼내지 않고 조회
+        System.out.println("queue.peek() = " + queue.peek());
+
+        //데이터 꺼내기
+        System.out.println("queue.poll() = " + queue.poll());
+        System.out.println("queue.poll() = " + queue.poll());
+        System.out.println("queue.poll() = " + queue.poll());
+        System.out.println(queue);
+    }
+}
